0a8673ea2ab0fd17e57193a889fb9e78788c75a8
yonk <nopctoday@gmail.com>
2018-04-17 10:28:45 +0200

    lowered gym requirement

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
     packages=['gym_minigrid', 'gym_minigrid.envs'],
     install_requires=[
-        'gym>=0.9.6',
+        'gym>=0.7.4',
         'numpy>=1.10.0',
         'pyqt5>=5.10.1'
     ]
